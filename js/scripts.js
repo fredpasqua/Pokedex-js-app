e2b34abb5ee0bdbf454d24e3b5d8ccbae51e7ad1
fredpasqua <fredpasqua74@gmail.com>
2021-12-19 17:31:27 -0500

    Update scripts.js
    Added array of pokemonList and 3 objects with various properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'pizza';
-document.write (favoriteFood);
+let pokemonList = [
+{name: 'Bulbasaur',height: .7, type: ['grass', 'poison']},
+{name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+{name: 'Venesaur', height: 2, type: ['grass', 'poison']}
+];
