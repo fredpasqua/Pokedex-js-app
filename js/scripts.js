f012f6bf26f24823f98a0996995ce09fdd9282f3
fredpasqua <fredpasqua74@gmail.com>
2021-12-20 21:44:05 -0500

    Update scripts.js
    added for loop to write Pokemon heights to the DOM and checks for height to highlight special (tall) pokemon and adds text "wow, that's tall."

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ let pokemonList = [
 {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
 {name: 'Venesaur', height: 2, type: ['grass', 'poison']}
 ];
+
+document.write("How tall are they?");
+document.write("<br>");
+
+/*iterates through the pokemon array and prints the height of each
+and adds a line break.*/
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + '\'s' + ' height is ' +
+  pokemonList[i].height + '.');
+
+//checks the height and returns statement for large pokemon
+if (pokemonList[i].height >= 2) {
+    document.write('  Wow, that\'s big!');
+  }
+
+  document.write("<br>");
+}
