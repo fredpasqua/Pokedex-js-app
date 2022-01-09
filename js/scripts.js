9c3e7624e153cea22fe3f719837324767d4a4825
fredpasqua <fredpasqua74@gmail.com>
2022-01-09 11:30:55 -0500

    edits for clarity
    added pokemon logo to local folder
    edited JS script variables: renamed for clarity of purpose

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Creates a IIFE protected repository for POKEMON for the PokeDex project//
 let pokemonRepository = (function () {
 let pokemonList = [
-{name: 'Bulbasaur',height: .7, type: ['grass', 'poison']},
+{name: 'Bulbasaur', height: .7, type: ['grass', 'poison']},
 {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
 {name: 'Venesaur', height: 2, type: ['grass', 'poison']}
 ];
@@ -12,20 +12,19 @@ function getAll() {
   return pokemonList;
 }
 
-/*Function to create add li elements with pokemon names from the pokemonList array
-and adds buttons to the li with the names.*/
+/*Function to create add <li> elements with pokemon names from the pokemonList array
+and adds buttons to the <li> with the names.*/
 function addListItem(pokemon){
-  let pokemonList = document.querySelector('.pokemon-list');
-  let listpokemon = document.createElement("li");
+  let listCreator = document.querySelector('.pokemon-list');
+  let listItemCreator = document.createElement("li");
   let button = document.createElement("button");
   button.innerText = pokemon.name;
   button.classList.add('button-style');
-  listpokemon.appendChild(button);
-  pokemonList.appendChild(listpokemon);
+  listCreator.appendChild(listItemCreator);
+  listItemCreator.appendChild(button);
   button.addEventListener('click', function showDetails() {
-    console.log(pokemon.name);
-  });
-
+      console.log(pokemon.name);
+    });
   };
 
 return {
@@ -35,7 +34,7 @@ return {
 };
 })();
 
+
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
-
 });
